a56bd7bcf700db4f5ae79e26a27cad85a4cd4a3b
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2025-03-05 22:11:12 +0300

    updsting

---
+++ components/logo.jsx
@@ -0,0 +1,11 @@
+export default function Logo() {
+  return (
+    <div className="w-[40rem] h-[60vh] relative">
+      <iframe
+        src="https://my.spline.design/graphicart-0eee200d4a1031bade3ac88aac8e86d2/"
+        width="100%"
+        height="100%"
+      ></iframe>
+    </div>
+  );
+}
